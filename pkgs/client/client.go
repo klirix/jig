fcff6f304842866ff3c5f57419a877d444de2b8a
Askhat Saiapov <askhat.saiapov@gmail.com>
2024-05-31 02:44:36 +0200

    redeploy client

--- pkgs/client/client.go
+++ pkgs/client/client.go
@@ -148,7 +148,6 @@ func (t *TrackableReader) Read(p []byte) (n int, err error) {
 
 func updateConfigUsingToken(token string) Config {
 	strings.Split(token, "+")
-
 	newConfig := Config{
 		Endpoint: strings.Split(token, "+")[0],
 		Token:    strings.Split(token, "+")[1],
